99283c29e1b058ff9c37c92902c67044d9e219b9
SirineZoghlami <sirine.zoghlami@esprit.tn>
2023-08-03 02:54:34 +0100

    edit-profile-form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { FooterComponent } from './footer/footer.component';
 import { CandidatureOffreEmploisFormComponent } from './candidature-offre-emplois-form/candidature-offre-emplois-form.component';
 import { AjouterOffreFormComponent } from './ajouter-offre-form/ajouter-offre-form.component';
 import { ProfilePageComponent } from './profile-page/profile-page.component';
+import { EditProfileFormComponent } from './edit-profile-form/edit-profile-form.component';
 
 @NgModule({
   declarations: [
@@ -25,6 +26,7 @@ import { ProfilePageComponent } from './profile-page/profile-page.component';
     CandidatureOffreEmploisFormComponent,
     AjouterOffreFormComponent,
     ProfilePageComponent,
+    EditProfileFormComponent,
   ],
   imports: [
     BrowserModule,
