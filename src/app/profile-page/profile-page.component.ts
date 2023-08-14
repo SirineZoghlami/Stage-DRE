72dc4f023be3ab4280392fd6b47e6a84f97dda2c
SirineZoghlami <sirine.zoghlami@esprit.tn>
2023-08-14 10:59:15 +0100

    user-profile fixes

--- src/app/profile-page/profile-page.component.ts
+++ src/app/profile-page/profile-page.component.ts
@@ -1,10 +1,73 @@
 import { Component } from '@angular/core';
+import { OnInit } from '@angular/core';
+import { trigger, state, style, animate, transition } from '@angular/animations';
+
+
 
 @Component({
   selector: 'app-profile-page',
   templateUrl: './profile-page.component.html',
-  styleUrls: ['./profile-page.component.css']
-})
-export class ProfilePageComponent {
+  styleUrls: ['./profile-page.component.css'],
 
+
+animations: [
+  trigger('slideDown', [
+    state('void', style({ height: '0', opacity: '0' })),
+    state('*', style({ height: '*', opacity: '1' })),
+    transition('void <=> *', animate('300ms ease-in-out')),
+  ]),
+],
+})
+
+
+export class ProfilePageComponent {  isDropdownOpen: boolean = false;
+
+  toggleDropdown() {
+    this.isDropdownOpen = !this.isDropdownOpen;
+  }
+/*******************thabetk llmodifier ****************************/
+  showModifProfile = false;
+
+  toggleModifProfile() {
+    this.showModifProfile = !this.showModifProfile;
+  }
+
+  scrollToModifProfile() {
+    const modifProfileSection = document.getElementById('modifProfileSection');
+    if (modifProfileSection) {
+      modifProfileSection.scrollIntoView({ behavior: 'smooth' });
+    }
+  }
+  /****************************ytalaa lfouq**************************/
+
+
+  scrollup() {
+    const up = document.getElementById('interface1');
+    if (up) {
+      this.showModifProfile = !this.showModifProfile;
+      up.scrollIntoView({ behavior: 'smooth' });
+    }
+  }
+
+  importCV() {
+    const fileInput = document.createElement('input');
+    fileInput.type = 'file';
+    fileInput.accept = '.pdf';
+    fileInput.addEventListener('change', (event: Event) => {
+      const target = event.target as HTMLInputElement;
+      if (target && target.files && target.files.length > 0) {
+        const selectedFile = target.files[0];
+        if (selectedFile.type === 'application/pdf') {
+          // Here, you can perform actions with the selected PDF file.
+          // For example, you can upload it to a server or process it locally.
+          console.log('Selected PDF file:', selectedFile);
+        } else {
+          console.log('Please select a valid PDF file.');
+        }
+      }
+    });
+
+    // Trigger the file input click event
+    fileInput.click();
+  }
 }
