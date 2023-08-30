ee9ba5dcb73dac9e1d79efa931452538c1e75a10
azizgm1234 <140641043+azizgm1234@users.noreply.github.com>
2023-08-30 16:15:34 +0100

    aziz

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -35,6 +35,26 @@ export class FormComponent {
     { id: 6, name: 'JAVASCRIPT' },
     { id: 7, name: 'REACTJS' },
     { id: 8, name: 'C#' },
-    { id: 9, name: 'C/C++' }]
+    { id: 9, name: 'C/C++' },
+    { id: 10, name: 'Design Graphique' },
+    { id: 11, name: 'Travail en équipe' },
+    { id: 12, name: 'Responsable' },
+    { id: 13, name: 'Négociation' },
+    { id: 14, name: 'Gestion de projet' },
+    { id: 15, name: 'Machine learning' },
+    { id: 16, name: 'Gestion d\'équipe' },
+    { id: 17, name: 'Data science' },
+    { id: 18, name: 'Adobe Photoshop' },
+    { id: 19, name: 'Finance' },
+    { id: 20, name: 'Mécanique' },
+    { id: 21, name: 'Électronique' },
+    { id: 22, name: 'Mécatronique' },
+    { id: 23, name: 'Marketing digital' },
+    { id: 24, name: 'Symfony' },
+    { id: 25, name: 'Math skills' }
 
+  ]
+  calculateHeight() {
+    return `${40 + this.selectedOptions.length * 40}px`; // Adjust the values as needed
+  }
 }
